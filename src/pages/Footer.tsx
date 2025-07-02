119b11ebe364b17aaf631e96946c407c74d5ed57
Sohard Pratap Singh <68166286+sohardpratap@users.noreply.github.com>
2025-07-03 01:19:41 +0530

    Updated Footer.tsx

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -81,7 +81,7 @@ const Footer: React.FC = () => {
         </div>
 
         <div className="border-t border-gray-800 dark:border-gray-900 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2025 SPSLabs.in. All rights reserved.</p>
+          <p>&copy; 2025 SPS Labs. All rights reserved.</p>
         </div>
       </div>
     </footer>
